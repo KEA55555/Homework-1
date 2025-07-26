6c3d7ed56039fe7fe4c80bb03775ec9aedebc14e
KEA55555 <kosmoz29@yandex.ru>
2025-07-27 00:33:53 +0300

    Написан тест

--- Project_1/sandbox/src/main/java/Triangle.java
+++ Project_1/sandbox/src/main/java/Triangle.java
@@ -4,43 +4,18 @@ public class Triangle {
         double a = 3;
         double b = 4;
         double c = 5;
-    
+        double p = ((a + b + c) / 2);
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
         System.out.println("Периметр треугольника со сторонами " + a + " и " + b + " и " + c + " = " + trianglePerimeter(a, b, c));
-//        System.out.println(p(a, b, c));
+        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " и " + c + " = " + triangleArea(area));
     }
 
-
-    private static double trianglePerimeter(double a, double b, double c) {
+    public static double trianglePerimeter(double a, double b, double c) {
         return a + b + c;
     }
 
-    private static double p(double a, double b, double c) {
-        double p = ((a + b + c) / 2);
-        return p;
-    }
-
-    private static double S(double a, double b, double c, double p){
-        double S = (p * (p - a) * (p - b) * (p - c));
-        return S;
-    }
-
-    private static double triangleArea(double S){
-       double triangleArea = Math.sqrt(S);
-        return triangleArea;
-    }
-
-    static void triangleArea1(double S){
-        System.out.println("Площадь треугольника " + triangleArea(S));
+    public static double triangleArea(double area) {
+        return  area;
     }
 }
-
-//    private static void printTriangleArea(double a, double b, double c) {
-//        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + "и" + c + " = " + triangleArea(a, b, c));
-//    }
-//
-//    private static double triangleArea(double a, double b, double c) {
-//        return a * b * c;
-//    }
-//}
-// return Math.sqrt(p(p - a) (p - b) (p - c));
--- Project_1/sandbox/src/test/java/TriangleTests.java
+++ Project_1/sandbox/src/test/java/TriangleTests.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TriangleTests {
+
+    @Test
+    void canCalculatePerimeter(){
+    double result = Triangle.trianglePerimeter(8.0, 2.0, 3.0);
+        Assertions.assertEquals(13, result);
+    }
+
+    @Test
+    void canCalculateArea(){
+        double result = Triangle.triangleArea(5.0, 5.0, 6.0);
+        Assertions.assertEquals(12, result);
+    }
 }
