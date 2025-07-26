3bce7e4be8f8d888301342e996307e0d43f5ac4f
KEA55555 <kosmoz29@yandex.ru>
2025-07-26 23:10:49 +0300

    Функции

--- Project_1/sandbox/src/main/java/Triangle.java
+++ Project_1/sandbox/src/main/java/Triangle.java
@@ -1,23 +1,46 @@
 public class Triangle {
 
-    printTriangleHalfMeter(double p);
-    printTriangleArea(3.0, 4.0, 5.0);
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 4;
+        double c = 5;
+    
 
-    // формула полупериметра p
-    private static double triangleHalfMeter(double a, double b, double c) {
-        return ((a + b + c) / 2);
+        System.out.println("Периметр треугольника со сторонами " + a + " и " + b + " и " + c + " = " + trianglePerimeter(a, b, c));
+//        System.out.println(p(a, b, c));
     }
 
-    // формула Герона (квадратный корень)
-    private static double triangleAreaHeron(double a, double b, double c) {
-        return Math.sqrt(p(p - a) (p - b) (p - c));
+
+    private static double trianglePerimeter(double a, double b, double c) {
+        return a + b + c;
     }
 
-    private static void printTriangleArea(double a, double b, double c) {
-        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + "и" + c + " = " + triangleArea(a, b, c));
+    private static double p(double a, double b, double c) {
+        double p = ((a + b + c) / 2);
+        return p;
     }
 
-    private static double triangleArea(double a, double b, double c) {
-        return a * b * c;
+    private static double S(double a, double b, double c, double p){
+        double S = (p * (p - a) * (p - b) * (p - c));
+        return S;
+    }
+
+    private static double triangleArea(double S){
+       double triangleArea = Math.sqrt(S);
+        return triangleArea;
+    }
+
+    static void triangleArea1(double S){
+        System.out.println("Площадь треугольника " + triangleArea(S));
     }
 }
+
+//    private static void printTriangleArea(double a, double b, double c) {
+//        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + "и" + c + " = " + triangleArea(a, b, c));
+//    }
+//
+//    private static double triangleArea(double a, double b, double c) {
+//        return a * b * c;
+//    }
+//}
+// return Math.sqrt(p(p - a) (p - b) (p - c));
