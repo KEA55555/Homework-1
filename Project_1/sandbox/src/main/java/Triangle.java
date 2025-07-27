274a8c8e02baa402d619fa2446102b57a7955f7c
KEA55555 <kosmoz29@yandex.ru>
2025-07-27 13:57:32 +0300

    Написаны тесты для класса Triangle
    Создан класс Triangle с тремя свойствами, реализованы методы для вычисления периметра и площади треугольника по формуле Герона, созданы тесты для проверки работы методов.

--- Project_1/sandbox/src/main/java/Triangle.java
+++ Project_1/sandbox/src/main/java/Triangle.java
@@ -4,18 +4,17 @@ public class Triangle {
         double a = 3;
         double b = 4;
         double c = 5;
-        double p = ((a + b + c) / 2);
-        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
         System.out.println("Периметр треугольника со сторонами " + a + " и " + b + " и " + c + " = " + trianglePerimeter(a, b, c));
-        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " и " + c + " = " + triangleArea(area));
+        System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " и " + c + " = " + triangleArea(a, b, c));
     }
 
     public static double trianglePerimeter(double a, double b, double c) {
         return a + b + c;
     }
 
-    public static double triangleArea(double area) {
-        return  area;
+    public static double triangleArea(double a, double b, double c) {
+        double p = ((a + b + c) / 2);
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 }
